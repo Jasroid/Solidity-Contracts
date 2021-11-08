ae840512281802a27161af46f1d57f4115de3add
jasroid <jasroid@gmail.com>
2021-11-08 21:57:24 +0200

    initial commit

---
+++ Tokens/BEP20-OpenZeppelin.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.6.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol"
+
+contract SpaceTurds is ERC20{
+    constructor(uint256 intialSupply) public ERC20("Name of Token","TokenSymbol"){
+        _mint(msg.sender, intialSupply);
+    }
+}
