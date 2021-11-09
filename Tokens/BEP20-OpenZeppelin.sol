79d41e87e5d4bad60f8dd53195f5bc5a89dc528e
Mmetja P Kekana <jasroid@gmail.com>
2021-11-09 13:35:24 +0200

    add missing ;

--- Tokens/BEP20-OpenZeppelin.sol
+++ Tokens/BEP20-OpenZeppelin.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.8.6;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol"
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract SpaceTurds is ERC20{
     constructor(uint256 intialSupply) public ERC20("Name of Token","TokenSymbol"){
