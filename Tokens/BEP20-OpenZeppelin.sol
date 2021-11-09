3447132519c238c92e10ef618f37ec0d93db7a48
Mmetja P Kekana <jasroid@gmail.com>
2021-11-09 13:37:38 +0200

    Update BEP20-OpenZeppelin.sol

--- Tokens/BEP20-OpenZeppelin.sol
+++ Tokens/BEP20-OpenZeppelin.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.6;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract SpaceTurds is ERC20{
+contract ContractName is ERC20{
     constructor(uint256 intialSupply) public ERC20("Name of Token","TokenSymbol"){
         _mint(msg.sender, intialSupply);
     }
